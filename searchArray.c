4249707444e7e831cef632d4b730af21c35d7569
prasadsarwar <86279488+prasadsarwar@users.noreply.github.com>
2023-01-01 20:27:56 +0530

    Update searchArray.c

--- searchArray.c
+++ searchArray.c
@@ -1,72 +1,175 @@
-#include <stdio.h>
+ #include <stdio.h>
+ #include <stdlib.h>
 
-int main(){
-	//array creation
-	int size,i,key;
-	printf("Enter size of array");
-	scanf("%d",&size);
-	
-	int arr[size];
-	printf("Enter array elements\n");
-	for(i=0;i<size;i++){
-		scanf("%d",&arr[i]);
-	}
-	
-	// Display Array
-	printf("Entered array elements are:");
-	for(i=0;i<size;i++){
-		printf("%d\t",arr[i]);
-	}
-	printf("\n");
-	int ch,flag=0;
-	printf("Enter Searching Method\n");
-	printf("1 ; Linear search\n2:Binary search");
-	printf("\nEnter Choice\t");
-	scanf("%d",&ch);
-	
-	printf("\nEnter search element\t");
-	scanf("%d",&key);
-	switch(ch){
-		case 1:{
-			// Linear Search
-			for(i=0;i<size;i++){
-				if(arr[i] == key){
-					printf("element %d found at position %d\n",key,i+1);
-					flag=1;
-				}
-			}
-			if(flag==0){
-				printf("Element not found");
-			}
-		}
-			break;
-		case 2:{
-			//Binary Search
-			int low = 0;
-			int high = size;
-			
-			while(low<=high){
-				int mid = (low + high)/2;
-				if(arr[mid] == key){
-					printf("element %d found at position %d\n",key,mid+1);
-					flag = 1;
-					break;
-				}
-				else if(arr[mid]>key){
-					high = mid-1;
-				}
-				else{
-					low = mid+1;
-				}
-			}
-			if(flag == 0){
-				printf("Element not found\n");
-			}
-		}
-			break;
-		default:{ 
-			printf("Enter Valid choice");
-		}
-	}
-	return 0;
+
+ void Display(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("%d\t",Arr[iCnt]);
+    }
+    printf("\n");
 }
+
+ void BubbleSort(int Arr[], int iSize)
+{
+    int i = 0, j = 0;
+    int iTemp = 0; 
+
+    for(i = 1; i < iSize; i++)
+    {
+        for(j = 0; j < iSize - i; j++)
+        {
+            if(Arr[j] > Arr[j + 1])
+            {
+                iTemp = Arr[j];
+                Arr[j] = Arr[j+1];
+                Arr[j+1] = iTemp;
+            }
+        }
+    }
+}
+
+ int LinearSearch(int Arr[], int iSize, int iKey)
+ {
+    int iCnt = 0, iFlag = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == iKey)
+        {
+            iFlag = 1;
+            break;
+        }
+    }
+    if(iFlag == -1)
+    {
+        return iFlag;
+    }
+    else
+    {
+        return iCnt + 1;
+    }
+ }
+
+ int BinarySearch(int Arr[], int iSize, int iKey)
+ {
+    int iLow = 0, iMid = 0, iHigh = 0;
+    int iCnt = 0, iFlag = 0;
+
+    iLow = 0;
+    iHigh = iSize;
+
+    while (iLow <= iHigh)
+    {
+        iMid = (iLow + iHigh) / 2;
+
+        if(Arr[iMid] == iKey)
+        {
+            iFlag = 1;
+            break;
+        }
+        else if (iKey > Arr[iMid])
+        {
+            iLow = iMid + 1;
+        }
+        else
+        {
+            iHigh = iMid - 1;
+        }     
+    }
+
+    if(iFlag == 0)
+    {
+        return iFlag;
+    }
+    else
+    {
+        return iMid + 1;
+    }
+     
+ }
+
+
+ int main()
+ {
+    int iSize = 0, iCnt = 0, iKey = 0;
+    int iRet = 0, iChoice = -1;
+    int *ptr = NULL;
+
+    printf("Enter Size of array:\n");
+    scanf("%d",&iSize);
+
+    ptr = (int *)malloc(iSize * sizeof(int));
+
+    if(ptr == NULL)
+    {
+        printf("Unable to allocate memory...");
+        return -1;
+    }
+
+    printf("Enter array elements:\n");
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        scanf("%d", &ptr[iCnt]);
+    }
+    while (iChoice != 0)
+    {
+        printf("Enter element you want to search:\n");
+        scanf("%d", &iKey);
+
+        printf("Cnoose search method:\n\n");
+        printf("1: Linear search\n");
+        printf("2: Binary Search (array will be sorted)\n");
+        printf("3: Display\n");
+        printf("4: Exit\n");
+
+        scanf("%d",&iChoice);
+
+        switch (iChoice)
+        {
+        case 1:
+            iRet = LinearSearch(ptr, iSize, iKey);
+
+            if(iRet == 0)
+            {
+                printf("Element not found...\n");
+            }
+            else
+            {
+                printf("Element %d found at position %d\n", iKey, iRet);
+            }
+
+            break;
+        
+        case 2:
+            BubbleSort(ptr,iSize);
+
+            iRet = BinarySearch(ptr,iSize,iKey);
+
+            if(iRet == 0)
+            {
+                printf("Element not found...\n");
+            }
+            else
+            {
+                printf("Element %d found at position %d\n", iKey, iRet);
+            }
+            break;
+
+        case 3:
+            Display(ptr, iSize);
+            break;
+
+        default:
+            printf("Invalid choice...\n");
+            break;
+        }
+    }
+  
+    
+    return 0;
+ }
